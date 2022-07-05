297fa31b119cc1d9cd539b16b426a515a2348357
dmitrijLo <demilorenz@googlemail.com>
2022-07-05 23:54:31 +0200

    complete query datapoint. not tested..tests are needed..

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <string.h>
+
+using namespace std;
 
 // Lab@Home Pins
 //#define TX_PIN 5
@@ -8,15 +11,29 @@
 #define TX_PIN 14
 #define RX_PIN 13
 
-enum StateMachine { RESET, INIT, IDLE, READ, WRITE };
+struct dataPoint{
+  string name;
+  float value;
+  uint16_t address;
+  uint8_t length;
+};
+
+enum StateMachine { RESET, INIT, IDLE, READ, WRITE, LISTEN, ACK };
 StateMachine currentState = RESET;
 
 class Optolink {
   public:
+    unsigned long lastMillis{0};
+    unsigned long debugMillis{0};
+    uint8_t rxBuffer[255];
+    uint8_t rxMsgLen{0};
     HardwareSerial* stream = &Serial1;
-    uint16_t address = 0x00F8; // Gerätekennung zum Testen
-    unsigned long lastMillis = 0;
-    unsigned long debugMillis = 0;
+    struct dataPoint datapoints[4] = {
+      {"Gerätekennung", 0, 0x00F8, 0x02 },
+      {"temp_aussen", 0, 0x0800, 0x02 },
+      {"temp_kessel", 0, 0x0802, 0x02 },
+      {"temp_speicher", 0, 0x0804, 0x02}
+    };
 
     void clearInputStream(void){
        while(stream -> available() > 1) stream -> read();
@@ -72,15 +89,46 @@ void loop() {
       optolink.clearInputStream();
       break;
     case READ: {
-      uint8_t buff[] = {0x41, 0x05, 0x00, 0x01, 0x00, 0x00, 0x02, 0x00}; //Stelle 4,5,7 zunächst nur Platzhalter
-      buff[4] = (optolink.address >> 8) & 0xFF; // noch nicht ganz klar warum...
-      buff[5] = optolink.address & 0xFF; // noch nicht ganz klar warum...
+      uint8_t buff[] = {0x41, 0x05, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}; //Stelle 4,5,6,7 zunächst nur Platzhalter
+      buff[4] = (optolink.datapoints[0].address >> 8) & 0xFF; // noch nicht ganz klar warum...
+      buff[5] = optolink.datapoints[0].address & 0xFF; // noch nicht ganz klar warum...
+      buff[6] = optolink.datapoints[0].length;
       buff[7] = optolink.getChecksum(buff, sizeof(buff));
       optolink.stream -> write(buff, sizeof(buff));
       // als nächstes Einlesen der Antwort
+      optolink.rxMsgLen = 8 + optolink.datapoints[0].length;
+      optolink.lastMillis = millis();
+      currentState = ACK;
     } break;
     case WRITE:
       break;
+    case LISTEN: {
+      uint8_t index = 0;
+      if(optolink.stream -> available()){
+        uint8_t buff = optolink.stream -> read();
+        if(buff == 0x06){   // acknowledged
+          while(optolink.stream -> available() > 0 && index != optolink.rxMsgLen){
+            optolink.rxBuffer[index] = optolink.stream -> read();
+            ++index;
+          }
+          (optolink.rxBuffer[0] == 0x41) ? currentState = ACK : currentState = IDLE;
+
+        } else if(buff == 0x15){    // not acknowledged
+          currentState = IDLE;
+          optolink.clearInputStream();
+        }
+      } 
+      if(millis() - optolink.lastMillis > 1000UL){    // timeout
+        currentState = RESET;
+        optolink.clearInputStream();
+      } 
+    } break;
+    case ACK: {
+      const uint8_t buffer[] = {0x06};
+      optolink.stream -> write(buffer, sizeof(buffer));
+      optolink.lastMillis = millis();
+      currentState = IDLE;
+    } break;
   }
 
   optolink.debugPrinter();
