70d1ca16783eefd97ba86ab5780357f08bcfdafe
ayoubziat <ayoub.ziat2204@gmail.com>
2022-07-26 19:40:09 +0200

    cleaned up the master branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,7 @@
 #include <Arduino.h>
 
-// Lab@Home Pins
-//#define TX_PIN 5
-//#define RX_PIN 6
-
-// esp32 NodeMCU
-#define TX_PIN 10
-#define RX_PIN 9
-
-//HardwareSerial* mySerial = &Serial1;
-
-//const uint8_t buff[] = {0x16, 0x00, 0x00};
-
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(4800, SERIAL_8E2, RX_PIN, TX_PIN); // RX, TX
-  delay(2500);
-  
 }
 
 void loop() {
-  //const uint8_t buff[] = {0x16, 0x00, 0x00};
-  //Serial1.write(buff, sizeof(buff));
- 
-
-
-  if(Serial1.available() > 1){
-    Serial.println();
-    Serial.print("Serial Data is available! "); Serial.print("The Message is "); Serial.print(Serial1.read());
-  }else{
-    Serial.print("No Data received...");
-  }
-
-  delay(2500);
-  
 }
