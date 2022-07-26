f759339ac100f40923c7dae0f69c7bcaaa4f40eb
ayoubziat <ayoub.ziat2204@gmail.com>
2022-07-26 19:38:38 +0200

    Refined the receive & send simulation code

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,34 @@
 #define TX_PIN 10
 #define RX_PIN 9
 
-//HardwareSerial* mySerial = &Serial1;
+HardwareSerial* mySerial = &Serial1;
 
-//const uint8_t buff[] = {0x16, 0x00, 0x00};
+void send();
+void receive();
 
 void setup() {
   Serial.begin(9600);
   Serial1.begin(4800, SERIAL_8E2, RX_PIN, TX_PIN); // RX, TX
   delay(2500);
-  
 }
 
 void loop() {
-  //const uint8_t buff[] = {0x16, 0x00, 0x00};
-  //Serial1.write(buff, sizeof(buff));
- 
+  //send();
+  receive();
 
+  delay(2500);
+}
 
+void send(){
+  const uint8_t buff[] = {0x16, 0x00, 0x00};
+  Serial1.write(buff, sizeof(buff));
+}
+
+void receive(){
   if(Serial1.available() > 1){
     Serial.println();
     Serial.print("Serial Data is available! "); Serial.print("The Message is "); Serial.print(Serial1.read());
   }else{
     Serial.print("No Data received...");
   }
-
-  delay(2500);
-  
 }
